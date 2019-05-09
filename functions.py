5a33c89e26d4ae6234c1d8e1aa974cba889f7a7c
Raksha <rakshapawar10.sae@gmail.com>
2019-05-09 21:18:31 +0530

    How to define and use functions

---
+++ functions.py
@@ -0,0 +1,18 @@
+def square(x):
+    return x * x
+
+def cube(x):
+    return x * x * x
+
+def main():
+    for i in range(10):
+        print("{} squared is {}".format(i, square(i)))
+
+    print("#############################################")
+
+    for i in range(10):
+        print("{} cubed is {}".format(i, cube(i)))
+
+
+if __name__ == "__main__":
+    main()
