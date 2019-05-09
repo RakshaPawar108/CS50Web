fad94e425afde98695bae28fbe5ab990cdd62e04
Raksha <rakshapawar10.sae@gmail.com>
2019-05-09 20:51:58 +0530

    Looping over sequences and simple looping

---
+++ loops0.py
@@ -0,0 +1,12 @@
+for i in range(10):
+    print(i)
+
+names = ["Alice", "Raksha", "Charlie", "Bob", "Lilly"]
+for name in names:
+    print(name)
+
+
+name1 = "Raksha"
+for i in name1:
+    print(i)
+    
