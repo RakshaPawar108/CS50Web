d20e4e73adb190ef381ff11e883fb2bcf513eb6c
Raksha <rakshapawar10.sae@gmail.com>
2019-05-10 19:41:29 +0530

    Isitnewyear site

---
+++ Webapp/application1.py
@@ -0,0 +1,12 @@
+import datetime                     #to get current date easily
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    now = datetime.datetime.now()           #getting current date and time and store it in time
+    new_year = now.month == 1 and now.day == 1          #bool variable new_year will check if date and month are true and will store value
+    new_year = True
+    return render_template("newyear.html", new_year=new_year)
